d66401ba579f9e14dbdb6d702f7061037037489f
Surya Madhavan <surya.madhavan@capgemini.com>
2023-03-16 15:22:36 +0530

    Data Ingestion working with Customer/Sales

--- data-ingestion/src/main/java/org/pl/entities/Sales.java
+++ data-ingestion/src/main/java/org/pl/entities/Sales.java
@@ -2,7 +2,7 @@ package org.pl.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.time.Instant;
+import java.util.Date;
 
 public class Sales {
 
@@ -10,7 +10,7 @@ public class Sales {
     private int salesId;
 
     @JsonProperty
-    private Instant timestamp;
+    private Date timestamp;
 
     @JsonProperty
     private int productId;
@@ -27,7 +27,7 @@ public class Sales {
     @JsonProperty
     private String productCategory;
 
-    public Sales(int salesId, Instant timestamp, int productId, int customerId, String productCategory, int quantity, int price) {
+    public Sales(int salesId, Date timestamp, int productId, int customerId, String productCategory, int quantity, int price) {
         this.salesId = salesId;
         this.timestamp = timestamp;
         this.productId = productId;
@@ -41,4 +41,72 @@ public class Sales {
 
     }
 
+    @Override
+    public String toString() {
+        return "Sales{" +
+                "salesId=" + salesId +
+                ", timestamp=" + timestamp +
+                ", productId=" + productId +
+                ", customerId=" + customerId +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", productCategory='" + productCategory + '\'' +
+                '}';
+    }
+
+    public int getSalesId() {
+        return salesId;
+    }
+
+    public void setSalesId(int salesId) {
+        this.salesId = salesId;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp (Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
 }
--- data-ingestion/src/main/java/org/pl/producer/ProducerNew.java
+++ data-ingestion/src/main/java/org/pl/producer/ProducerNew.java
@@ -3,6 +3,7 @@ package org.pl.producer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -13,36 +14,65 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.pl.entities.Customer;
+import org.pl.entities.Sales;
 import org.pl.serde.JSONSerde;
 import org.pl.utils.ProducerUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class ProducerNew {
 
-    private static Customer parseCustomer(JSONObject custObj){
-        Gson gson = new Gson();
-        Customer customer1 = gson.fromJson(String.valueOf(custObj), Customer.class);
-//        customer1.setCustomerId(Integer.parseInt((String) customer.get("customerId")));
-//        customer1.setAge(Integer.parseInt((String) customer.get("age")));
-//        customer1.setGender((String) customer.get("gender"));
-//        customer1.setName((String) customer.get("name"));
-//        System.out.println(customer1.getName());
+    static Gson gson = new Gson();
+
+    private static Customer parseCustomer(JSONObject customerObj){
+        Customer customer1 = gson.fromJson(String.valueOf(customerObj), Customer.class);
         return customer1;
     }
-    public static List<Customer> readFromFile(){
+    private static Sales parseSales(JSONObject salesObj){
+        Sales sales1 = gson.fromJson(String.valueOf(salesObj), Sales.class);
+        return sales1;
+    }
+
+    public static List<Sales> readSalesFromFile(){
         JSONParser jsonParser =  new JSONParser();
-        try(FileReader reader = new FileReader("data-ingestion\\src\\main\\resources\\customerData.json")){
+        try(FileReader salesReader = new FileReader("data-ingestion\\src\\main\\resources\\salesData.json")) {
 
-            Object obj = jsonParser.parse(reader);
-            JSONArray customerList = (JSONArray) obj;
+            Object salesObj = jsonParser.parse(salesReader);
+            JSONArray salesList = (JSONArray) salesObj;
+            List<Sales> allSales = (List<Sales>) salesList
+                    .stream()
+                    .map(sale -> parseSales((JSONObject) sale))
+                    .collect(Collectors.toList());
+            return allSales;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return  null;
+    }
+    public static List<Customer> readCustomersFromFile(){
+        JSONParser jsonParser =  new JSONParser();
+        try(FileReader customerReader = new FileReader("data-ingestion\\src\\main\\resources\\customerData.json"); FileReader salesReader = new FileReader("data-ingestion\\src\\main\\resources\\salesData.json")){
 
-            List<Customer> allCustomers = (List<Customer>) customerList.stream().map(customer -> parseCustomer((JSONObject) customer)).collect(Collectors.toList());
+            Object customerObj = jsonParser.parse(customerReader);
+            JSONArray customerList = (JSONArray) customerObj;
+            List<Customer> allCustomers = (List<Customer>) customerList
+                    .stream()
+                    .map(customer -> parseCustomer((JSONObject) customer))
+                    .collect(Collectors.toList());
+
+
             return allCustomers;
         } catch (Exception e){
             e.printStackTrace();
@@ -64,15 +94,23 @@ public class ProducerNew {
         Properties properties = ProducerUtils.loadProperties();
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JSONSerde.class);
-//        read json file
-        List<Customer> allCustomers = readFromFile();
+        // read json file
+        List<Customer> allCustomers = readCustomersFromFile();
+        List<Sales> allSales = readSalesFromFile();
         // send data
         try(Admin adminClient = Admin.create(properties);
             org.apache.kafka.clients.producer.Producer<Integer, JsonNode> producer = new KafkaProducer<>(properties)) {
-            final String inputTopic = properties.getProperty("input.customer.topic");
-            List<ProducerRecord<Integer,JsonNode>> records = allCustomers.stream().map(customer -> new ProducerRecord<>(inputTopic, customer.getCustomerId(), mapper.convertValue(customer, JsonNode.class))).collect(Collectors.toList());
+            final String inputCustomerTopic = properties.getProperty("input.customer.topic");
+            final String inputSalesTopic = properties.getProperty("input.sales.topic");
+            List<ProducerRecord<Integer,JsonNode>> records = allCustomers.stream()
+                    .map(customer -> new ProducerRecord<>(inputCustomerTopic, customer.getCustomerId(), mapper.convertValue(customer, JsonNode.class)))
+                    .collect(Collectors.toList());
+            records.addAll(allSales.stream()
+                    .map(sale ->
+                            new ProducerRecord<>(inputSalesTopic, sale.getSalesId(), mapper.convertValue(sale, JsonNode.class)))
+                    .collect(Collectors.toList()));
             records.forEach(record -> producer.send(record, callback));
-//            adminClient.
+            producer.close(Duration.ofSeconds(5));
         }
     }
 }
